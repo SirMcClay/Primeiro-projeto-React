12d1ca970367f24a2cfeb9782913cd050d1969c8
sirmcclay <mcclay.design@gmail.com>
2020-07-14 12:16:50 -0300

    Dashboard styling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/52668639?s=460&v=4"
+            alt="Sir McClay"
+          />
+          <div>
+            <strong>repositorio/mcclay</strong>
+            <p>Essa é uma descrição placeholder para desenvolvimento</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/52668639?s=460&v=4"
+            alt="Sir McClay"
+          />
+          <div>
+            <strong>repositorio/mcclay</strong>
+            <p>Essa é uma descrição placeholder para desenvolvimento</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/52668639?s=460&v=4"
+            alt="Sir McClay"
+          />
+          <div>
+            <strong>repositorio/mcclay</strong>
+            <p>Essa é uma descrição placeholder para desenvolvimento</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
